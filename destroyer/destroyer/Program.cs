275c89fc6e0bdeb71388e9db3d9eee3b9a478077
vromero12 <vromero@mcbain.org>
2016-11-08 09:39:37 -0500

    beta
    its in a beta form

--- destroyer/destroyer/Program.cs
+++ destroyer/destroyer/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace destroyer
+namespace JusGame
 {
-	class MainClass
+	class Program 
 	{
-		public static void Main (string[] args)
+		static void Main(string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string yourName;
+			Console.WriteLine("What is your name?");
+
+			yourName = Console.ReadLine();
+
+			if (yourName=="Gabe")
+			{
+				Console.WriteLine("you expire go to the depth of the underworld {0}", yourName);
+			}
+			else
+			{
+				Console.WriteLine("live you must!"); 
+				while (true)
+					while (true) {
+						Console.Write ("Do you want to play again [Y/N]?");
+						string answer = Console.ReadLine ().ToUpper ();
+						if (answer == "Y")
+							break; 
+						if (answer == "N")
+							return;}
+						
+			}
 		}
 	}
 }
