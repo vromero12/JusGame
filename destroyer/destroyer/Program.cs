f55132e6f42dd9f1dcfc59b71a51c0fc8d6b48b1
vromero12 <vromero@mcbain.org>
2016-11-14 15:58:42 -0500

    fix stuff
    fix stuff

--- destroyer/destroyer/Program.cs
+++ destroyer/destroyer/Program.cs
@@ -7,26 +7,24 @@ namespace JusGame
 		static void Main(string[] args)
 		{
 			string yourName;
-			Console.WriteLine("What is your name?");
+			while (true) {
+				Console.WriteLine ("What is your name?");
 
-			yourName = Console.ReadLine();
+				yourName = Console.ReadLine ();
 
-			if (yourName=="Gabe")
-			{
-				Console.WriteLine("you expire go to the depth of the underworld {0}", yourName);
-			}
-			else
-			{
-				Console.WriteLine("live you must!"); 
-				while (true)
+				if (yourName == "Gabe") {
+					Console.WriteLine ("you expire go to the depth of the underworld {0}", yourName);
+				} else {
+					Console.WriteLine ("live you must!"); 
 					while (true) {
 						Console.Write ("Do you want to play again [Y/N]?");
 						string answer = Console.ReadLine ().ToUpper ();
 						if (answer == "Y")
 							break; 
 						if (answer == "N")
-							return;}
-						
+							return;
+					}
+				}
 			}
 		}
 	}
